34beced6b04b2625fc2a813024ffd69f3f38fd69
PiharyovDmitro <dimonm99@gmail.com>
2024-02-21 21:49:24 +0200

    finish

--- goit-algo-hw-04/support_bot.py
+++ goit-algo-hw-04/support_bot.py
@@ -1,6 +1,3 @@
-#словарь для зберігання контактів
-contacts = {}
-
 #функція додавання контакту
 def add_contact(name, phone):
     contacts[name] = phone
@@ -31,6 +28,36 @@ def show_all_contacts():
     else:
         return 'No contacts saved.'
 
-    
+def parse_input(user_input):
+    cmd, *args = user_input.split()
+    cmd = cmd.strip().lower()
+    return cmd, *args
 
-    
+def main():
+    contacts = {}
+
+    print("Welcome to the assistant bot!")
+    print("Available commands:\nhello - greet\nadd [name] [phone] - add a contact\nchange [name] [new phone] - update a contact\nphone [name] - show phone number\nall - show all contacts\nclose or exit - close the bot")
+
+    while True:
+        user_input = input("Enter a command: ")
+        command, *args = parse_input(user_input)
+
+        if command in ["close", "exit"]:
+            print("Good bye!")
+            break
+        elif command == 'add':
+            print(add_contact(args, contacts))
+        elif command == 'change':
+            print(change_contact(args, contacts))
+        elif command == 'phone':
+            print(show_phone(args, contacts))
+        elif command == 'all':
+            print(show_all_contacts())
+        elif command == "hello":
+            print("How can I help you?")
+        else:
+            print("Invalid command.")
+
+if __name__ == "__main__":
+    main()
