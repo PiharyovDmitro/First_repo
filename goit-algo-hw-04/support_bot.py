44a4f06cb9cdba689c70e3cc3871b0209099003c
PiharyovDmitro <dimonm99@gmail.com>
2024-02-21 21:12:38 +0200

    bot_1

---
+++ goit-algo-hw-04/support_bot.py
@@ -0,0 +1,40 @@
+#словарь для зберігання контактів
+contacts = {}
+
+#функція привітання
+def hello():
+    return 'How can I help you?'
+
+#функція додавання контакту
+def add_contact(name, phone):
+    contacts[name] = phone
+    return 'Contact added.'
+
+#функция зміни номеру
+def change_contact(name, new_phone):
+    if name in contacts:
+        contacts[name] = new_phone
+        return 'Contact updated.'
+    else:
+        return f'Contact with {name} not found.'
+
+#функція виводу номера телефона за ім'ям
+def show_phone(name):
+    if name in contacts:
+        return contacts[name]
+    else:
+        return f'Contact with {name} not found.'
+
+#функція виведення усіх контактів
+def show_all_contacts():
+    if contacts:
+        all_contacts = 'All contacts:\n'
+        for name, phone in contacts.items():
+            all_contacts += f'{name}: {phone}\n'
+        return all_contacts
+    else:
+        return 'No contacts saved.'
+
+    
+
+    
