1d15bdd204711a93b6616818f7220c5bb87b564e
PiharyovDmitro <dimonm99@gmail.com>
2024-02-28 19:32:35 +0200

    Flush

--- main.py
+++ main.py
@@ -1,3 +1,14 @@
-import datetime
-now = datetime.datetime.now()
-print(now)
+def logger(func):
+    def inner(x: int, y: int) -> int:
+        print(f"Викликається функція: {func.__name__}: {x}, {y}")
+        result = func(x, y)
+        print(f"Функція {func.__name__} завершила виконання: {result}")
+        return result
+
+    return inner
+
+@logger
+def complicated(x: int, y: int) -> int:
+    return x + y
+
+print(complicated(2, 3))
