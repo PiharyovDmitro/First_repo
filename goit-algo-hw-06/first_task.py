94b9fbdfe1d9227852e84002bb9ec122474766c5
PiharyovDmitro <dimonm99@gmail.com>
2024-03-23 13:11:06 +0200

    homework

---
+++ goit-algo-hw-06/first_task.py
@@ -0,0 +1,76 @@
+import re
+
+class Field:
+    def __init__(self, value):
+        self.value = value
+
+class Name(Field):
+    def __init__(self, value):
+        super().__init__(value)
+
+class Phone(Field):
+    def __init__(self, value):
+        super().__init__(value)
+        self.validate()
+
+    def validate(self):
+        if not re.match(r'^\d{10}$', self.value):
+            raise ValueError("Invalid phone number format")
+
+class Record:
+    def __init__(self, name):
+        self.name = Name(name)
+        self.phones = []
+
+    def add_phone(self, phone):
+        self.phones.append(Phone(phone))
+
+    def remove_phone(self, phone):
+        self.phones = [p for p in self.phones if p.value != phone]
+
+    def edit_phone(self, old_phone, new_phone):
+        for p in self.phones:
+            if p.value == old_phone:
+                p.value = new_phone
+                p.validate()
+                break
+
+    def find_phone(self, phone):
+        return [p for p in self.phones if p.value == phone]
+
+class AddressBook:
+    def __init__(self):
+        self.records = []
+
+    def add_record(self, record):
+        self.records.append(record)
+
+    def search_by_name(self, name):
+        return [record for record in self.records if record.name.value == name]
+
+    def delete_record(self, name):
+        self.records = [record for record in self.records if record.name.value != name]
+
+
+# Приклад використання
+if __name__ == "__main__":
+    address_book = AddressBook()
+
+    # Додавання записів
+    record1 = Record("John Doe")
+    record1.add_phone("1234567890")
+    address_book.add_record(record1)
+
+    record2 = Record("Jane Smith")
+    record2.add_phone("9876543210")
+    record2.add_phone("5554443333")
+    address_book.add_record(record2)
+
+    # Пошук записів за іменем
+    search_result = address_book.search_by_name("John Doe")
+    for record in search_result:
+        print("Name:", record.name.value)
+        print("Phones:", [phone.value for phone in record.phones])
+
+    # Видалення записів за іменем
+    address_book.delete_record("Jane Smith")
--- secondmain.py
+++ secondmain.py
@@ -1,17 +1,1 @@
-def format_string(string, length):
-    if len(string) >= length:
-        return string
-    else:
-        spaces_before = (length - len(string)) // 2
-        spaces_after = length - len(string) - spaces_before
-        if spaces_before < spaces_after:
-            spaces_after -= 1
-        formatted_string = " " * spaces_before + string + " " * spaces_after
-        return formatted_string
-    
-
-# Приклад виклику функції
-input_string = 'abba'
-formatted_result = format_string(input_string, 15)
-print(f"Original String: '{input_string}'")
-print(f"Formatted String: '{formatted_result}'")
+__new__
