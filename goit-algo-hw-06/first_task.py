fc48b01b14ce504202207aeec35bd31ca1cd801c
PiharyovDmitro <dimonm99@gmail.com>
2024-03-31 00:00:40 +0200

    зкфмлш

--- goit-algo-hw-06/first_task.py
+++ goit-algo-hw-06/first_task.py
@@ -20,7 +20,7 @@ class Phone(Field):
 
 class Record:
     def __init__(self, name):
-        self.name = name
+        self.name = Name(name)
         self.phones = []
 
     def add_phone(self, phone):
@@ -30,21 +30,24 @@ class Record:
         self.phones = [p for p in self.phones if p.value != phone]
 
     def edit_phone(self, old_phone, new_phone):
-        for p in self.phones:
-            if p.value == old_phone:
-                p.value = new_phone
-                p.validate()
-                break
+        if old_phone not in [p.value for p in self.phones]:
+            raise ValueError("Phone number {} not found in the record.".format(old_phone))
 
     def find_phone(self, phone):
-        return [p for p in self.phones if p.value == phone]
+        for p in self.phones:
+            if p.value == phone:
+                return p
+        return None
 
 class AddressBook(UserDict):
     def add_record(self, record):
         self.data[record.name.value] = record
 
     def find(self, name):
-        return [record for record_name, record in self.data.items() if record_name == name]
+        for record in self.data.values():
+            if record.name.value == name:
+                return record
+        return None
 
     def delete(self, name):
         del self.data[name]
